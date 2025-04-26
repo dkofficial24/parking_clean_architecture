e4f388bda078d74370a23cb233c9aae80dc9b081
Dinesh <dkofficial24@gmail.com>
2025-04-26 13:55:37 +0530

    - enhance: moved dio in separate class dio_client - added interceptor for request and response and error tracking

---
+++ lib/parking/core/network/dio_client.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:parking_demo/parking/core/constant/api_endpoints.dart';
+
+@lazySingleton
+class DioClient {
+  DioClient() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: ApiEndpoints.baseUrl,
+        connectTimeout: const Duration(seconds: 15),
+        receiveTimeout: const Duration(seconds: 15),
+        responseType: ResponseType.json,
+      ),
+    );
+
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (option, handler) {
+          option.headers = {'Authorization': 'Bearer mysecrettoken123'};
+          return handler.next(option);
+        },
+        onResponse: (response, handler) {
+          return handler.next(response);
+        },
+        onError: (DioException exception, handler) {
+          print(exception.toString());
+          return handler.next(exception);
+        },
+      ),
+    );
+  }
+
+  late Dio _dio;
+
+  Dio get dio => _dio;
+}
