1f0c18ad7c56f3cfd2589e6eb43149a05ef1eadd
Dinesh <dkofficial24@gmail.com>
2025-04-26 21:30:19 +0530

    chore: updated linting rules

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:parking_demo/parking/presentation/bloc/parking_bloc.dart';
 import 'package:parking_demo/parking/presentation/bloc/parking_events.dart';
-import 'package:parking_demo/parking/presentation/bloc/parking_states.dart';
 
+import 'localization/localization.i69n.dart';
 import 'parking/core/di/di_injection.dart';
 import 'parking/presentation/views/available_booking_slots_screen.dart';
 
@@ -17,19 +17,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: const Localization().title,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: BlocProvider<ParkingBloc>(
-        create: (_)=>getIt<ParkingBloc>()..add(FetchedParking()),
-        child: AvailableSlotsScreen()
+        create: (_) => getIt<ParkingBloc>()..add(FetchedParking()),
+        child: const AvailableSlotsScreen(),
       ),
     );
   }
 }
-
